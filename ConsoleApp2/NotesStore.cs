ba5dcac6caa48dc540cfcb141ec08c19b2fcfa92
Ershad Raoufi <ershadraoufi@gmail.com>
2022-09-01 22:01:25 +0430

    Add project files.

---
+++ ConsoleApp2/NotesStore.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Item
+    {
+        public string Key { get; set; }
+        public string value { get; set; }
+    }
+    public class NotesStore
+    {
+        List<string> states = new List<string>() { "active", "others", "completed" };
+        List<Item> _notes = new List<Item>();
+        public NotesStore() { }
+        public void AddNote(String state, String name) {
+            if(string.IsNullOrEmpty(name))
+                throw new Exception("Name cannot be empty");
+            if (!states.Contains(state))
+                throw new ArgumentException($"Invalid state {state}");
+            _notes.Add(new Item() { Key = state,value = name });
+
+        }
+        public List<String> GetNotes(String state) {
+            if (!states.Contains(state))
+                throw new ArgumentException($"Invalid state {state}");
+            if (!_notes.Any(x => x.Key == state))
+                return new List<string>();
+            return _notes.Where(x => x.Key == state).Select(x=>x.value).ToList();
+        }
+
+        public class Employee
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string Company { get; set; }
+        }
+    }
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿using static ConsoleApp2.NotesStore;
+
+public class Program
+{
+    public static Dictionary<string, int> AverageAgeForEachCompany(List<Employee> employees)
+    {
+        var dic = new Dictionary<string, int>();
+        var componies = employees.Select(x => x.Company).Distinct().ToList();
+        foreach (var com in componies)
+        {
+            var age = employees
+                .Where(x => x.Company == com)
+                .Average(x => x.Age);
+            dic.Add(com, Convert.ToInt32(age));
+        }
+        return dic.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+    }
+
+    public static Dictionary<string, int> CountOfEmployeesForEachCompany(List<Employee> employees)
+    {
+        var dic = new Dictionary<string, int>();
+        var componies = employees.Select(x => x.Company).Distinct().ToList();
+        foreach (var com in componies)
+        {
+            var age = employees
+                .Where(x => x.Company == com);
+            dic.Add(com, age.Count());
+        }
+        return dic.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+    }
+
+    public static Dictionary<string, Employee> OldestAgeForEachCompany(List<Employee> employees)
+    {
+        var dic = new Dictionary<string, Employee>();
+        var componies = employees.Select(x => x.Company).Distinct().ToList();
+        foreach (var com in componies)
+        {
+            var age = employees
+                .Where(x => x.Company == com);
+            var oldest = age.Max(x => x.Age);
+            var emp = employees.First(x => x.Age == oldest);
+            dic.Add(com, emp);
+        }
+        return dic.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+    }
+    public static void Main()
+    {
+        
+    }
+}
